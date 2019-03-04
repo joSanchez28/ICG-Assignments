3369c143e6026b751efdd1735573984f78cc8f1e
Ncharly <charli557@gmail.com>
2019-03-04 14:39:57 +0100

    Update Plane.cpp

--- Assignment 0 - Raytracing-HelloWorld/assignment_0/src/Plane.cpp
+++ Assignment 0 - Raytracing-HelloWorld/assignment_0/src/Plane.cpp
@@ -26,40 +26,6 @@ Plane::Plane(const vec3& _center, const vec3& _normal)
 
 
 //-----------------------------------------------------------------------------
-/*
-// Jorge:
-
-bool
-Plane::
-intersect(const Ray& _ray,
-          vec3&      _intersection_point,
-          vec3&      _intersection_normal,
-          double&    _intersection_t ) const
-{
-	// /** \todo
-	// * - compute the intersection of the plane with `_ray`
-	// * - if ray and plane are parallel there is no intersection
-	// * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
-	// * - return whether there is an intersection in front of the viewer (t > 0)
-	//
-	bool intersection = false;
-
-	//Are ray and  plane parallel? If yes; then there is no intersection by indications.
-	if (dot(normal, _ray.direction) != 0) {
-		//Compute the intersection data and store it:
-		_intersection_t = (-dot(normal, _ray.origin) + norm(center)) / dot(normal, _ray.direction); //Note: norm(center) = distance(center, (0,0,0))
-		_intersection_point = _ray.origin + _intersection_t * _ray.direction;
-		_intersection_normal = (_intersection_point - center) + normal;
-
-		if (_intersection_t > 0) {
-			intersection = true;
-		}
-	}
-
-    return intersection;
-}
-*/
-// Carlos:
 
 bool
 Plane::
@@ -98,7 +64,4 @@ intersect(const Ray& _ray,
     }
 
 }
-
-
-
 //=============================================================================
