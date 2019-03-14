acc373d5f0bd323002c111377d422c3f9dc898a1
Ncharly <charli557@gmail.com>
2019-03-14 20:42:51 +0100

    Update Mesh.cpp

--- Assignment 0 - Raytracing-HelloWorld/assignment_0/src/Mesh.cpp
+++ Assignment 0 - Raytracing-HelloWorld/assignment_0/src/Mesh.cpp
@@ -15,7 +15,7 @@
 #include <string>
 #include <stdexcept>
 #include <limits>
-
+#include "Plane.h"
 
 //== IMPLEMENTATION ===========================================================
 
@@ -143,6 +143,30 @@ void Mesh::compute_normals()
         v.normal = vec3(0,0,0);
     }
 
+    /** \todo
+     * In some scenes (e.g the office scene) some objects should be flat
+     * shaded (e.g. the desk) while other objects should be Phong shaded to appear
+     * realistic (e.g. chairs). You have to implement the following:
+     * - Compute vertex normals by averaging the normals of their incident triangles.
+     * - Store the vertex normals in the Vertex::normal member variable.
+     * - Weigh the normals by their triangles' angles.
+     */
+
+    double w0, w1, w2;
+    vec3 wn;
+    for (Triangle& t: triangles_){
+        angleWeights(vertices_[t.i0].position, vertices_[t.i1].position, vertices_[t.i2].position, w0, w1, w2);
+        wn = vec3(w0, w1, w2);
+        vertices_[t.i0].normal = vertices_[t.i0].normal + wn[0]*t.normal;
+        vertices_[t.i1].normal = vertices_[t.i1].normal + wn[1]*t.normal;  
+        vertices_[t.i2].normal = vertices_[t.i2].normal + wn[2]*t.normal;
+        
+    }
+    
+    for (Vertex& v: vertices_)
+    {
+        v.normal = v.normal/norm(v.normal);
+    }
 }
 
 
@@ -168,9 +192,51 @@ void Mesh::compute_bounding_box()
 bool Mesh::intersect_bounding_box(const Ray& _ray) const
 {
 
+    /** \todo
+    * Intersect the ray `_ray` with the axis-aligned bounding box of the mesh.
+    * Note that the minimum and maximum point of the bounding box are stored
+    * in the member variables `bb_min_` and `bb_max_`. Return whether the ray
+    * intersects the bounding box.
+    * This function is ued in `Mesh::intersect()` to avoid the intersection test
+    * with all triangles of every mesh in the scene. The bounding boxes are computed
+    * in `Mesh::compute_bounding_box()`.
+    */
 
-    return true;
+    vec3 nx = vec3(1,0,0);
+    vec3 ny = vec3(0,1,0);
+    vec3 nz = vec3(0,0,1);
+    vec3 intersectionpoint;
+    vec3 intersectionnormal;
+    double t;
+    bool inters[6];
+
+    Plane(bb_min_, nx).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    inters[0] = (intersectionpoint[1]>=bb_min_[1] && intersectionpoint[1]<=bb_max_[1] && intersectionpoint[2]>=bb_min_[2] && intersectionpoint[2]<=bb_max_[2]);
+    
+    Plane(bb_max_, nx).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    inters[1] = (intersectionpoint[1]>=bb_min_[1] && intersectionpoint[1]<=bb_max_[1] && intersectionpoint[2]>=bb_min_[2] && intersectionpoint[2]<=bb_max_[2]);
+    
+    Plane(bb_min_, ny).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    inters[2] = (intersectionpoint[0]>=bb_min_[0] && intersectionpoint[0]<=bb_max_[0] && intersectionpoint[2]>=bb_min_[2] && intersectionpoint[2]<=bb_max_[2]);
+    
+    Plane(bb_max_, ny).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    inters[3] = (intersectionpoint[0]>=bb_min_[0] && intersectionpoint[0]<=bb_max_[0] && intersectionpoint[2]>=bb_min_[2] && intersectionpoint[2]<=bb_max_[2]);
+    
+    Plane(bb_min_, nz).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    inters[4] = (intersectionpoint[0]>=bb_min_[0] && intersectionpoint[0]<=bb_max_[0] && intersectionpoint[1]>=bb_min_[1] && intersectionpoint[1]<=bb_max_[1]);
+    
+    //We have to check only 6-1 faces, since when a ray intersect in one face it has to go out through another (not the same)
+    //Plane(bb_max_, nz).intersect(_ray, intersectionpoint, intersectionnormal, t);
+    //inters[5] = (intersectionpoint[0]>=bb_min_[0] && intersectionpoint[0]<=bb_max_[0] && intersectionpoint[1]>=bb_min_[1] && intersectionpoint[1]<=bb_max_[1]);
+
+    if((inters[0] || inters[1] || inters[2] || inters[3] || inters[4]) && t>0){
+        return (true);
+    }
+    else{
+        return(false);
+    }
 }
+
 
 
 //-----------------------------------------------------------------------------
@@ -228,8 +294,61 @@ intersect_triangle(const Triangle&  _triangle,
     const vec3& p1 = vertices_[_triangle.i1].position;
     const vec3& p2 = vertices_[_triangle.i2].position;
 
+    /** \todo
+    * - intersect _ray with _triangle
+    * - store intersection point in `_intersection_point`
+    * - store ray parameter in `_intersection_t`
+    * - store normal at intersection point in `_intersection_normal`.
+    * - Depending on the member variable `draw_mode_`, use either the triangle
+    *  normal (`Triangle::normal`) or interpolate the vertex normals (`Vertex::normal`).
+    * - return `true` if there is an intersection with t > 0 (in front of the viewer)
+    *
+    * Hint: Rearrange `ray.origin + t*ray.dir = a*p0 + b*p1 + (1-a-b)*p2` to obtain a solvable
+    * system for a, b and t.
+    * Refer to [Cramer's Rule](https://en.wikipedia.org/wiki/Cramer%27s_rule) to easily solve it.
+     */
 
-    return false;
+    vec3 a, b, c, d;
+    double den, alpha, beta, gamma, t;
+
+    a = p1-p0;
+    b = p2-p0;
+    c = -_ray.direction;
+    d = _ray.origin-p0;
+    // Denominator
+    den = (a[0]*b[1]*c[2]+a[1]*b[2]*c[0]+b[0]*c[1]*a[2]-c[0]*b[1]*a[2]-a[1]*b[0]*c[2]-b[2]*c[1]*a[0]); 
+
+    //Cramer
+    // Check if ray is // to the triangle plane
+    if(den!=0){
+        beta = (d[0]*b[1]*c[2]+d[1]*b[2]*c[0]+b[0]*c[1]*d[2]-c[0]*b[1]*d[2]-d[1]*b[0]*c[2]-b[2]*c[1]*d[0])/den;
+        gamma = (a[0]*d[1]*c[2]+a[1]*d[2]*c[0]+d[0]*c[1]*a[2]-c[0]*d[1]*a[2]-a[1]*d[0]*c[2]-d[2]*c[1]*a[0])/den;
+        t = (a[0]*b[1]*d[2]+a[1]*b[2]*d[0]+b[0]*d[1]*a[2]-d[0]*b[1]*a[2]-a[1]*b[0]*d[2]-b[2]*d[1]*a[0])/den;
+        alpha = 1 - beta - gamma;
+        if (alpha>=0 && beta>=0 && gamma>=0 && t>0){
+            /*_intersection_point[0] = alpha*p0[0]+beta*p1[0]+gamma*p2[0];
+            _intersection_point[1] = alpha*p0[1]+beta*p1[1]+gamma*p2[1];
+            _intersection_point[2] = alpha*p0[2]+beta*p1[2]+gamma*p2[2];
+            equals to: */
+            _intersection_point = _ray.origin + t*_ray.direction;
+            _intersection_t = t;
+            if(draw_mode_==FLAT){
+                _intersection_normal = _triangle.normal;
+            }
+            else{
+                _intersection_normal = normalize(alpha*vertices_[_triangle.i0].normal + beta*vertices_[_triangle.i1].normal + gamma*vertices_[_triangle.i2].normal);
+            }
+
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+    else{
+
+        return(false);
+    }
 }
 
 
